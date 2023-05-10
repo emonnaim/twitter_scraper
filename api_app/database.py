cdd1126e67b0a7b5ee718bce349f4bd3c5eb9076
mouh2020 <badimohammed2019@gmail.com>
2023-05-10 19:32:53 +0100

    change the structure

--- api_app/database.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import SQLModel, create_engine
-from config import *
-
-postgres_url = f"{db_type}://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
-engine = create_engine(postgres_url)
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
