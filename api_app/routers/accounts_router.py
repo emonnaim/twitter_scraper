15a5f51944a3fa51e6c0cd0c88a9c3b63dca3905
mouh2020 <badimohammed2019@gmail.com>
2023-05-10 18:35:17 +0100

    fix the return of add accounts

--- api_app/routers/accounts_router.py
+++ api_app/routers/accounts_router.py
@@ -25,17 +25,21 @@ def get_accounts_by_channel_Id(channel_id : int,session : Session = Depends(get_
 
 @router.post('/{channel_id}',response_model=list[Account])
 def add_accounts_to_channel_id(channel_id : int,usernames : list[str] ,session : Session = Depends(get_session)) : 
-    for username in usernames : 
-        try : 
-            account = session.exec(select(Account).where(Account.username == username)).first()
-            if account : 
-                continue
-            account = Account(username=username)
-            commit_changes(session,list[account])
-            session.autoflush()
-        except Exception as e : 
-            pass
-    return session.get(Channel,channel_id).accounts
+    channel = session.get(Channel,channel_id)
+    if channel == None : 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Channel Not Found")
+    else : 
+        for username in usernames : 
+            try : 
+                account = session.exec(select(Account).where(Account.username == username)).first()
+                if account : 
+                    continue
+                account = Account(username=username,account_channel=channel)
+                commit_changes(session,[account])
+                session.refresh(channel)
+            except Exception as e : 
+                pass
+        return channel.accounts
 @router.delete('/{channel_id}',response_model=list[Account])
 def delete_accounts_by_channel_ids(channel_id : int,usernames : list[str],session : Session = Depends(get_session)) : 
     for username in usernames : 
