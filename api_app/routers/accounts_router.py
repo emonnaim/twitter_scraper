11904b88ae508a4073452615c2b2d149cd09e4ac
mouh2020 <badimohammed2019@gmail.com>
2023-05-10 18:14:26 +0100

    set routers

--- api_app/routers/accounts_router.py
+++ api_app/routers/accounts_router.py
@@ -1,23 +1,48 @@
 from dependencies.database import get_session,commit_changes
-from models.account import Account
+from models.account import Account,AccountRead
+from models.channel import Channel
 from sqlmodel import Field, Session, SQLModel, select
 from fastapi import Depends, HTTPException, Query,status,Response,APIRouter
 
 router = APIRouter(prefix="/accounts",tags=["Accounts"])
 
 
-@router.get("")
-def get_accounts(*,session : Session = Depends(get_session)) : 
-    pass
+@router.get("",response_model=list[AccountRead])
 
-@router.get('/{channel_id}')
-def get_accounts_by_group_Id(*,channel_id : int,session : Session = Depends(get_session)) : 
-    pass
+def get_accounts(session : Session = Depends(get_session)) : 
+    fetch_statement = select(Account)
+    accounts = session.exec(fetch_statement).all()
+    print(accounts)
+    return accounts
 
-@router.post('/{channel_id}')
-def add_accounts_to_channel_id(*,channel_id : int,session : Session = Depends(get_session)) : 
-    pass
+@router.get('/{channel_id}',response_model=list[Account])
+def get_accounts_by_channel_Id(channel_id : int,session : Session = Depends(get_session)) : 
+    try : 
+        return session.get(Channel,channel_id).accounts
+    except Exception as e : 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Channel Not Found")
 
-@router.delete('/{channel_id}')
-def delete_accounts_by_channel_ids(*,channel_id : int,session : Session = Depends(get_session)) : 
-    pass
+
+@router.post('/{channel_id}',response_model=list[Account])
+def add_accounts_to_channel_id(channel_id : int,usernames : list[str] ,session : Session = Depends(get_session)) : 
+    for username in usernames : 
+        try : 
+            account = session.exec(select(Account).where(Account.username == username)).first()
+            if account : 
+                continue
+            account = Account(username=username)
+            commit_changes(session,list[account])
+            session.autoflush()
+        except Exception as e : 
+            pass
+    return session.get(Channel,channel_id).accounts
+@router.delete('/{channel_id}',response_model=list[Account])
+def delete_accounts_by_channel_ids(channel_id : int,usernames : list[str],session : Session = Depends(get_session)) : 
+    for username in usernames : 
+        try : 
+            account = session.exec(select(Account).where(Account.username == username)).first()
+            session.delete(account)
+            session.commit()
+        except Exception as e : 
+            pass
+    return session.get(Channel,channel_id).accounts
--- api_app/routers/apis_router.py
+++ api_app/routers/apis_router.py
@@ -1,18 +1,33 @@
 from dependencies.database import get_session,commit_changes
-from models.api import API
+from models.api import API,APICreate,APIRead
 from sqlmodel import Field, Session, SQLModel, select
 from fastapi import Depends, HTTPException, Query,status,Response,APIRouter
 
 router = APIRouter(prefix="/apis",tags=["API"])
 
-@router.get("")
-def get_apis(*,session : Session = Depends(get_session)) : 
-    pass
+@router.get("",response_model=list[APIRead])
+def get_apis(*,session : Session = Depends(get_session)) :
+    fetch_statement = select(API)
+    apis = session.exec(fetch_statement).all()
+    return apis
 
-@router.post('/{channel_id}')
-def add_api(channel_id : int,session : Session = Depends(get_session)) : 
-    pass
+@router.post('',response_model=APIRead)
+def add_api(api : APICreate,session : Session = Depends(get_session)) : 
+    try : 
+        api  = API.from_orm(api)
+        commit_changes(session,[api])
+        return api
+    except Exception as e: 
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,detail=e)
+
 
 @router.delete('/{api_id}')
 def delete_api(api_id : int,session : Session = Depends(get_session)) : 
-    pass
+
+    api =  session.get(API,api_id)
+    if api == None : 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="API Not Found")
+    session.delete(api)
+    session.commit()
+    
+    
